06ef33ae4a8ed99c656b7c68729255ef12742737
CallumWolfe <callumw118@callumw118-System-Product-Name.home>
2020-12-16 14:33:44 +0000

    Adds middleware grouping for a date and time route

--- src/public/index.php
+++ src/public/index.php
@@ -15,9 +15,27 @@ $config['db']['pass']   = 'password';
 $config['db']['dbname'] = 'exampleapp';
 
 $app = new \Slim\App(['settings' => $config]);
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args){
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+// Grouping Middleware
+
+$app->group('/utils', function () use ($app) {
+    $app->get('/date', function ($request, $response) {
+        return $response->getBody()->write(date('Y-m-d'));
+    });
+    $app->get('/time', function ($request, $response) {
+        return $response->getBody()->write(date('H:i:s'));
+    });
+})->add(function ($request, $response, $next) {
+    $response->getBody()->write('It is now ');
+    $response = $next($request, $response);
+    $response->getBody()->write('. Enjoy!');
 
     return $response;
 });
