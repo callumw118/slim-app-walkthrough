4a532618c5592df019c040d43588da52fe9e8476
CallumWolfe <callumw118@callumw118-System-Product-Name.home>
2020-12-16 13:54:23 +0000

    adds configuration for app and database variables

--- src/public/index.php
+++ src/public/index.php
@@ -4,7 +4,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
+// Config for app
+$config['displayErrorDetails'] = true;
+$config['addContentLengthheader'] = false;
+
+//Config for Database
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'user';
+$config['db']['pass']   = 'password';
+$config['db']['dbname'] = 'exampleapp';
+
+$app = new \Slim\App(['settings' => $config]);
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args){
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
@@ -12,4 +22,3 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response;
 });
 $app->run();
-
